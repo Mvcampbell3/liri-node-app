d36dd4f7cc2940b81d8395866b0e4b8dee296f5d
Mvcampbell3 <mvcampbell3@gmail.com>
2019-02-01 03:10:42 -0500

    movie-this now works

--- liri.js
+++ liri.js
@@ -2,19 +2,31 @@ require("dotenv").config();
 const keys = require("./keys.js");
 const Spotify = require("node-spotify-api");
 const spotify = new Spotify(keys.spotify);
+const omdbKey = keys.omdb.key;
+const axios = require("axios");
+// console.log(keys.omdb.key)
 
 // console.log(process.argv);
 
 switch (process.argv[2]) {
     case "spotify-this-song":
-        let searchTerm = [];
-        for (let i = 3; i < process.argv.length; i++){
-            searchTerm.push(process.argv[i]);
+        let songTitle = [];
+        for (let i = 3; i < process.argv.length; i++) {
+            songTitle.push(process.argv[i]);
         }
-        searchTerm = searchTerm.join(" ");
-        runSpotify(searchTerm);
+        songTitle = songTitle.join(" ");
+        runSpotify(songTitle);
         break;
-    default: 
+    case "movie-this":
+        let movieTitle = [];
+        for (let i = 3; i < process.argv.length; i++) {
+            movieTitle.push(process.argv[i]);
+        }
+        movieTitle = movieTitle.join("+");
+        // console.log(movieTitle);
+        runOMDB(movieTitle);
+        break;
+    default:
         console.log("this is not working as expected");
 }
 
@@ -46,4 +58,36 @@ function runSpotify(song) {
         console.log("Preview link for Spotify: " + answer.external_urls.spotify);
 
     });
+};
+
+function runOMDB(movie) {
+    // console.log(movie);
+    // console.log(omdbKey);
+    axios.get("http://www.omdbapi.com/?apikey=" + omdbKey + "&t=" + movie).then(function (response) {
+        // console.log(response.data);
+        let info = response.data;
+        console.log("-------------------------------");
+        console.log("Movie Title: "+info.Title);
+        console.log("-------------------------------");
+        let year = info.Released.split(" ");
+        console.log("Release Year: " + year[2]);
+        console.log("-------------------------------");
+        console.log(info.Ratings[0].Source + " Rating: " + info.Ratings[0].Value);
+        console.log("-------------------------------");
+        console.log(info.Ratings[1].Source + " Rating: " + info.Ratings[1].Value);
+        console.log("-------------------------------");
+        console.log("Country of Production: "+info.Country);
+        console.log("-------------------------------");
+        console.log("Language: "+info.Language);
+        console.log("-------------------------------");
+        console.log("Plot: "+info.Plot);
+        console.log("-------------------------------");
+        console.log("Actors: "+info.Actors);
+        console.log("-------------------------------");
+
+
+
+    }).catch(function(err){
+        // console.log(err);
+    })
 }
