26aa73a11969ba4b06258a49e5d8b31e5aa89cf1
Mvcampbell3 <mvcampbell3@gmail.com>
2019-02-01 03:35:00 -0500

    added default searched for movie-this and spotify-this-song

--- liri.js
+++ liri.js
@@ -4,27 +4,33 @@ const Spotify = require("node-spotify-api");
 const spotify = new Spotify(keys.spotify);
 const omdbKey = keys.omdb.key;
 const axios = require("axios");
-// console.log(keys.omdb.key)
-
-// console.log(process.argv);
 
 switch (process.argv[2]) {
     case "spotify-this-song":
-        let songTitle = [];
-        for (let i = 3; i < process.argv.length; i++) {
-            songTitle.push(process.argv[i]);
+        if (process.argv.length > 3) {
+            let songTitle = [];
+            for (let i = 3; i < process.argv.length; i++) {
+                songTitle.push(process.argv[i]);
+            }
+            songTitle = songTitle.join(" ");
+            runSpotify(songTitle);
+        } else {
+            let sign = "The Sign"
+            runSpotify(sign);
         }
-        songTitle = songTitle.join(" ");
-        runSpotify(songTitle);
         break;
     case "movie-this":
-        let movieTitle = [];
-        for (let i = 3; i < process.argv.length; i++) {
-            movieTitle.push(process.argv[i]);
+        if (process.argv.length > 3) {
+            let movieTitle = [];
+            for (let i = 3; i < process.argv.length; i++) {
+                movieTitle.push(process.argv[i]);
+            }
+            movieTitle = movieTitle.join("+");
+            runOMDB(movieTitle);
+        } else {
+            let nobody = "Mr. Nobody"
+            runOMDB(nobody);
         }
-        movieTitle = movieTitle.join("+");
-        // console.log(movieTitle);
-        runOMDB(movieTitle);
         break;
     default:
         console.log("this is not working as expected");
@@ -33,20 +39,12 @@ switch (process.argv[2]) {
 
 
 
-
-
-
-
-
-
-// Functions for running the different switches
+// Functions for running the different process.argv[2]
 
 
 function runSpotify(song) {
-    spotify.search({ type: 'track', query: song }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
+    spotify.search({ type: 'track', query: song }, (err, data) => {
+        if (err) throw err;
         let answer = data.tracks.items[0];
         console.log("-----------------------------");
         console.log("Song Title: " + answer.name);
@@ -56,18 +54,14 @@ function runSpotify(song) {
         console.log("Album Name: " + answer.album.name);
         console.log("-----------------------------");
         console.log("Preview link for Spotify: " + answer.external_urls.spotify);
-
     });
 };
 
 function runOMDB(movie) {
-    // console.log(movie);
-    // console.log(omdbKey);
-    axios.get("http://www.omdbapi.com/?apikey=" + omdbKey + "&t=" + movie).then(function (response) {
-        // console.log(response.data);
+    axios.get("http://www.omdbapi.com/?apikey=" + omdbKey + "&plot=short&t=" + movie).then((response) => {
         let info = response.data;
         console.log("-------------------------------");
-        console.log("Movie Title: "+info.Title);
+        console.log("Movie Title: " + info.Title);
         console.log("-------------------------------");
         let year = info.Released.split(" ");
         console.log("Release Year: " + year[2]);
@@ -76,18 +70,13 @@ function runOMDB(movie) {
         console.log("-------------------------------");
         console.log(info.Ratings[1].Source + " Rating: " + info.Ratings[1].Value);
         console.log("-------------------------------");
-        console.log("Country of Production: "+info.Country);
+        console.log("Country of Production: " + info.Country);
         console.log("-------------------------------");
-        console.log("Language: "+info.Language);
+        console.log("Language: " + info.Language);
         console.log("-------------------------------");
-        console.log("Plot: "+info.Plot);
+        console.log("Plot: " + info.Plot);
         console.log("-------------------------------");
-        console.log("Actors: "+info.Actors);
+        console.log("Actors: " + info.Actors);
         console.log("-------------------------------");
-
-
-
-    }).catch(function(err){
-        // console.log(err);
-    })
+    }).catch((err) => { console.log(err) });
 }
