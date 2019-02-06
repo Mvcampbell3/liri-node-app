1d09b8090e4c92df9f8d1e6c028f3bdea0f4f6e8
Mvcampbell3 <mvcampbell3@gmail.com>
2019-02-05 23:13:02 -0500

    trying to add things to the readme

--- README.md
+++ README.md
@@ -1,1 +1,110 @@
-# Liri Node App
+# Liri Node Application
+
+Welcome to my <span style="color:green">**Liri App!**</span><br>
+This <span style="color:blue">**README**</span> will walk you through how this app works!
+
+# Table of Contents:
+1. [Description](#desc)
+2. [Installation](#install)
+3. [Usage](#use)
+4. [Examples](#example)
+
+<a name="desc"></a>
+## 1. Description:
+This application serves a few purposes at once.
+- First: It will give the user the ability to use various commands to receive information.
+- Second: It will demonstrate my ability to use technologies such as :<br>
+    - Node.js<br>
+    - Node Package Manager<br>
+    - Axios<br>
+    - Moment.js<br>
+    - Spotify, OMDB, and BandsInTown APIs<br>
+    - Inquirer, cli-table, and colors packages
+- Third: It will be fun!
+
+<a name="install"></a>
+## 2. Installation:
+Gee, that all sounds well and good Michael. How do we use Liri?<br>
+**Excellent Question**, but first we have some work to do.<br>
+
+Once you have successfully installed the packages in the folder with you cloned the app with
+```
+    npm install
+```
+And have created a .env file containing your API keys
+```
+# Spotify API keys
+
+SPOTIFY_ID=["something super secret"]
+SPOTIFY_SECRET=["something even more super secret"]
+
+# OMDB API key
+
+omdbKey=["kind of secret, I guess"]
+
+# Bands in Town app id
+
+band_app_id=["Good luck with this one"]
+```
+
+Congratulations! You are now ready to use the app!!
+
+<a name="use"></a>
+## 3. Usage:
+
+Every usage of liri will start with 
+```
+node liri.js
+```
+This is getting **exciting** now!
+
+From here we will be prompted with four options:
+- Song Details
+- Movie Facts
+- Band/Artist Concert Information
+- You Decide For Me
+
+You will use the up and down arrow keys to pick your selection, then press *_Enter_*
+
+Last Step!
+
+You then need to add the name of whatever it is your are searching for.<br>
+That's it!!<br>
+Now Liri will scour the API's and return your information.
+
+Here is a breakdown of what to expect:
+- Song Details (You will receive up to the top 5 results)
+```
+Name of the Song.
+Name of the Artist.
+Name of the Album.
+Link to listen on Spotify.
+```
+- Movie Facts
+```
+Title of the movie.
+Year the movie came out.
+IMDB Rating of the movie.
+Rotten Tomatoes Rating of the movie.
+Country where the movie was produced.
+Language of the movie.
+Plot of the movie.
+Actors in the movie.
+```
+- Band/Artist Concert Information (You will receive up to the next 15 shows)
+```
+Name of the venue.
+Where the venue is located.
+The date of the event
+```
+- You Decide For Me
+    - This will run though my random.txt file and grab an item from it. It could be a movie, a show, or a song!
+
+<a name="example"></a>
+## 4. Examples:
+[![Watch the video]("./images/liriDemo.webm)
+<video controls="controls">
+  <source type="video/mp4" src="filename.mp4"></source>
+  <source type="video/webm" src="filename.webm"></source>
+  <p>Your browser does not support the video element.</p>
+</video>
--- liri.js
+++ liri.js
@@ -17,7 +17,7 @@ inquirer.prompt(
     {
         type: "list",
         name: "method",
-        message: "What would you like to learn today",
+        message: "What would you like to learn today".green,
         choices: [
             "Song Details",
             "Movie Facts",
@@ -32,7 +32,7 @@ inquirer.prompt(
                 {
                     type: "input",
                     name: "songName",
-                    message: "Which song would you like to know more about?"
+                    message: "Which song would you like to know more about?".green
                 }
             ).then((answer) => {
                 if (answer.songName != "") {
@@ -47,7 +47,7 @@ inquirer.prompt(
                 {
                     type: "input",
                     name: "movieName",
-                    message: "Which movie would you like to know more about?"
+                    message: "Which movie would you like to know more about?".green
                 }
             ).then((answer) => {
                 if (answer.movieName != "") {
@@ -62,7 +62,7 @@ inquirer.prompt(
                 {
                     type: "input",
                     name: "bandName",
-                    message: "Which band/artist would you like to know about their upcoming shows?"
+                    message: "Which band/artist would you like to know about their upcoming shows?".green
                 }
             ).then((answer) => {
                 if (answer.bandName != "") {
@@ -101,13 +101,13 @@ function runSpotify(song) {
 
         songStructure.forEach((song) => {
             let displaySong = new Table({
-                head: ["Song Detail", "Information"]
+                head: ["Song Detail".green, "Information".green]
             });
             displaySong.push(
-                { "Song Title": song.name },
-                { "Band/Artist": song.artist },
-                { "Album Name": song.album },
-                { "Listen": song.listen }
+                { "Song Title": song.name.cyan },
+                { "Band/Artist": song.artist.cyan },
+                { "Album Name": song.album.cyan },
+                { "Listen": song.listen.cyan }
             );
             console.log(displaySong.toString());
         });
@@ -120,15 +120,15 @@ function runOMDB(movie) {
     axios.get("http://www.omdbapi.com/?apikey=" + omdbKey + "&plot=short&t=" + movie).then((response) => {
         let info = response.data;
         console.log("-------------------------------".red);
-        console.log("Movie Title: ".green + info.Title.blue);
+        console.log("Movie Title: ".green + info.Title.cyan);
         let year = info.Released.split(" ");
-        console.log("Release Year: ".green + year[2].blue);
-        console.log(info.Ratings[0].Source.green + " Rating: ".green + info.Ratings[0].Value.blue);
-        console.log(info.Ratings[1].Source.green + " Rating: ".green + info.Ratings[1].Value.blue);
-        console.log("Country of Production: ".green + info.Country.blue);
-        console.log("Language: ".green + info.Language.blue);
-        console.log("Plot: ".green + info.Plot.blue);
-        console.log("Actors: ".green + info.Actors.blue);
+        console.log("Release Year: ".green + year[2].cyan);
+        console.log(info.Ratings[0].Source.green + " Rating: ".green + info.Ratings[0].Value.cyan);
+        console.log(info.Ratings[1].Source.green + " Rating: ".green + info.Ratings[1].Value.cyan);
+        console.log("Country of Production: ".green + info.Country.cyan);
+        console.log("Language: ".green + info.Language.cyan);
+        console.log("Plot: ".green + info.Plot.cyan);
+        console.log("Actors: ".green + info.Actors.cyan);
         console.log("-------------------------------".red);
 
     }).catch((err) => console.log("There was an error with your search, please check your spelling and try again".red));
@@ -149,12 +149,12 @@ function runBandsInTown(band) {
             newArray.forEach((event) => {
                 let show = moment(event.date).format("MM/DD/YYYY");
                 let displayShow = new Table({
-                    head: ["Concert Detail", "Infortmation"]
+                    head: ["Concert Detail".green, "Infortmation".green]
                 })
                 displayShow.push(
-                    { "Name of Venue": event.name },
-                    { "Venue Location": event.place },
-                    { "Date of Show": show },
+                    { "Name of Venue": event.name.cyan },
+                    { "Venue Location": event.place.cyan },
+                    { "Date of Show": show.cyan },
                 )
                 console.log(displayShow.toString());
             })
